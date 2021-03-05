57d3d6f671ead31675e8b93b44dcf30df91e2042
kevin <nghe0001@algonquinlive.com>
2021-03-04 17:18:40 -0800

    about

---
+++ pages/about/about.js
@@ -0,0 +1,13 @@
+import Layout from '../../components/Layout'
+
+
+export default function about() {
+
+    return (
+        <Layout title='About NextJS Movie Project'
+        description= 'my NextJs Description'>
+        
+         <h1>First Post</h1>
+        </Layout>
+    )
+  }
